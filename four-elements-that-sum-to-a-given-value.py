5ed999eddcf62e2d0ebfaa066904a8d45146f935
Tutoryal <tutoryal.data@gmail.com>
2022-06-04 20:28:51 +0530

    first commit

--- four-elements-that-sum-to-a-given-value.py
+++
@@ -1,1 +0,0 @@
-https://www.geeksforgeeks.org/find-four-elements-that-sum-to-a-given-value-set-2/
--- from pytube import YouTube.py
+++
@@ -1,9 +0,0 @@
-# pip install pytube
-
-from pytube import YouTube
-
-video_link = "https://www.youtube.com/watch?v=35EQXmHKZYs"
-
-video = YouTube(video_link)
-
-video.streams.filter(res='720p').first().download('E:\\videos')
